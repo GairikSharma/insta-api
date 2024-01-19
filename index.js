981ed8fc423565975f08e46b496652178d398b90
GairikSharma <gairiksharma202@gmail.com>
2024-01-20 02:29:56 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const instagramDl = require("@sasmeee/igdl");
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello from the server...");
+});
+
+//Insta reel downloader
+app.get("/insta-reel-download", async (req, res) => {
+  try {
+    const url = req.body.url;
+    const dataList = await instagramDl(url);
+    res.json({ message: "Success", data: dataList });
+  } catch (error) {
+    console.log("Error occured", error);
+  }
+});
+
+
+app.listen(4000, (req, res) => {
+  console.log("Server running...");
+});
